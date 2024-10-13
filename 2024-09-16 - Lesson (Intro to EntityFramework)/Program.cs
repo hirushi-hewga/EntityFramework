24c3ac53900ae6f89fd5ad532c402f84aeccd5ac
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-10-13 10:11:04 -0700

    Add project files.

---
+++ 2024-09-16 - Lesson (Intro to EntityFramework)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2024_09_16___Lesson__Intro_to_EntityFramework_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
