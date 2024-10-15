6adb0c309fd670fcd0776e6ffc699fff105f19ed
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-10-15 09:04:07 -0700

    HW (IntroToEntityFramework)

--- 2024-09-18 - HW (Intro to EntityFramework)/MusicDbContext.cs
+++ 2024-09-18 - HW (Intro to EntityFramework)/MusicDbContext.cs
@@ -39,31 +39,64 @@ namespace _2024_09_18___HW__Intro_to_EntityFramework_
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasData(new Country[]
             {
-                new Country { Id = 1, Name = ""}
+                new Country { Id = 1, Name = "Canada" },
+                new Country { Id = 2, Name = "USA" },
+                new Country { Id = 3, Name = "United Kingdom" }
             });
             modelBuilder.Entity<Artist>().HasData(new Artist[]
             {
-                new Artist { Id = 1, Name = ""}
+                new Artist { Id = 1, Name = "Justin", Surname = "Bieber", CountryId = 1 },
+                new Artist { Id = 2, Name = "Taylor", Surname = "Swift", CountryId = 2 },
+                new Artist { Id = 3, Name = "Beyonce", Surname = "Knowles", CountryId = 2 },
+                new Artist { Id = 4, Name = "Ed", Surname = "Sheeran", CountryId = 3 },
+                new Artist { Id = 5, Name = "Dua", Surname = "Lipa", CountryId = 3 },
+                new Artist { Id = 6, Name = "Selena", Surname = "Gomez", CountryId = 2 },
             });
             modelBuilder.Entity<Genre>().HasData(new Genre[]
             {
-                new Genre { Id = 1, Name = ""}
+                new Genre { Id = 1, Name = "Pop"},
+                new Genre { Id = 2, Name = "R&B"},
+                new Genre { Id = 3, Name = "Pop/Folk"}
             });
             modelBuilder.Entity<Album>().HasData(new Album[]
             {
-                new Album { Id = 1, Name = ""}
+                new Album { Id = 1, Name = "Purpose", ArtistId = 1, 
+                    YearOfRelease = new DateOnly(2015,11,13), GenreId = 1 },
+                new Album { Id = 2, Name = "1989", ArtistId = 2, 
+                    YearOfRelease = new DateOnly(2014,10,27), GenreId = 1 },
+                new Album { Id = 3, Name = "Lemonade", ArtistId = 3, 
+                    YearOfRelease = new DateOnly(2016,4,23), GenreId = 2 },
+                new Album { Id = 4, Name = "+ (Divide)", ArtistId = 4, 
+                    YearOfRelease = new DateOnly(2017,3,3), GenreId = 3 },
+                new Album { Id = 5, Name = "FutureNostalgia", ArtistId = 5, 
+                    YearOfRelease = new DateOnly(2020,3,27), GenreId = 1 },
+                new Album { Id = 6, Name = "Rare", ArtistId = 6, 
+                    YearOfRelease = new DateOnly(2020,1,10), GenreId = 1 }
             });
             modelBuilder.Entity<Track>().HasData(new Track[]
             {
-                new Track { Id = 1, Name = ""}
+                new Track { Id = 1, Name = "What Do You Mean?", AlbumId = 1, Duration = new TimeOnly(0,3,25) },
+                new Track { Id = 2, Name = "Blank Space", AlbumId = 2, Duration = new TimeOnly(0,3,51) },
+                new Track { Id = 3, Name = "Formation", AlbumId = 3, Duration = new TimeOnly(0,3,26) },
+                new Track { Id = 4, Name = "Castle on the Hill", AlbumId = 4, Duration = new TimeOnly(0,4,21) },
+                new Track { Id = 5, Name = "Don`t Start Now", AlbumId = 5, Duration = new TimeOnly(0,3,3) },
+                new Track { Id = 6, Name = "Lose You to Love Me", AlbumId = 6, Duration = new TimeOnly(0,3,46) },
             });
             modelBuilder.Entity<Category>().HasData(new Category[]
             {
-                new Category { Id = 1, Name = ""}
+                new Category { Id = 1, Name = "Pop" },
+                new Category { Id = 2, Name = "R&B" },
+                new Category { Id = 3, Name = "Mood Music" },
+                new Category { Id = 4, Name = "Emotional Songs" },
+                new Category { Id = 5, Name = "Party" },
             });
             modelBuilder.Entity<Playlist>().HasData(new Playlist[]
             {
-                new Playlist { Id = 1, Name = ""}
+                new Playlist { Id = 1, Name = "Pop Hits", CategoryId = 1 },
+                new Playlist { Id = 2, Name = "R&B Selection", CategoryId = 2 },
+                new Playlist { Id = 3, Name = "Mood Melodies", CategoryId = 3 },
+                new Playlist { Id = 4, Name = "Emotional", CategoryId = 4 },
+                new Playlist { Id = 5, Name = "Party", CategoryId = 5 }
             });
         }
     }
@@ -101,7 +134,7 @@ namespace _2024_09_18___HW__Intro_to_EntityFramework_
         public int Id { get; set; }
         [Required,MaxLength(100)]
         public string Name { get; set; }
-        public DateTime YearOfRelease { get; set; }
+        public DateOnly YearOfRelease { get; set; }
         public Artist Artist { get; set; }
         public int ArtistId { get; set; }
         public Genre Genre { get; set; }
@@ -114,7 +147,7 @@ namespace _2024_09_18___HW__Intro_to_EntityFramework_
         public int Id { get; set; }
         [Required,MaxLength(100)]
         public string Name { get; set; }
-        public int Duration { get; set; }
+        public TimeOnly Duration { get; set; }
         public Album Album { get; set; }
         public int AlbumId { get; set; }
         public ICollection<Playlist> Playlists { get; set; }
