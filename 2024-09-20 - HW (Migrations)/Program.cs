53f11ee63f0d48294033657e05216db3758b2ea2
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-11-01 04:14:14 -0700

    HW (Migrations)

--- 2024-09-20 - HW (Migrations)/Program.cs
+++ 2024-09-20 - HW (Migrations)/Program.cs
@@ -1,37 +1,38 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks.Dataflow;
 using static System.Console;
 
 namespace _2024_09_20___HW__Migrations_
 {
     internal class Program
     {
+        public static void Tracks(MusicDbContext dbContext)
+        {
+            var tracks = dbContext.Tracks.Select(t => t);
+            double listeningAVG = dbContext.Tracks.Average(t => t.Quantity);
+            foreach (var item in tracks)
+                if (listeningAVG <= item.Quantity)
+                    WriteLine($"Name : {item.Name,-24}" +
+                              $"Duration : {item.Duration.Minute}:" +
+                                         $"{item.Duration.Second:00}    " +
+                              $"Quantity : {item.Quantity}");
+        }
         static void Main(string[] args)
         {
             MusicDbContext dbContext = new MusicDbContext();
 
-            #region 1
-
-            /*
-            var tracks = dbContext.Tracks.Where(t => t.Quantity >= 30000);
-            foreach (var item in tracks)
-            {
-                WriteLine($"Name : {item.Name,-24}" +
-                          $"Duration : {item.Duration.Minute}:" +
-                                     $"{item.Duration.Second:00}    " +
-                          $"Quantity : {item.Quantity}");
-            }
-            */
-
-            #endregion
+            // 1
+            Tracks(dbContext);
 
             #region 2
 
 
-            foreach (var item in dbContext.Artists)
-            {
-                WriteLine($"{item.Name} :");
-                var tracks = dbContext.Artists.Join(dbContext.Albums, ar => ar.Id, al => al.ArtistId, (ar, al) => new {  });
-            }
+            //foreach (var item in dbContext.Artists)
+            //{
+            //    WriteLine($"{item.Name} :");
+            //    var tracks = dbContext.Albums.Include(al => al.Artist)
+            //        .Include(ar => ar.Tracks).ToList();
+            //}
 
 
             #endregion
