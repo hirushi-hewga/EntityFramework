d5346c8c45c8dd2203c29b8ead6a2a19d67ece26
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-11-05 11:16:14 -0800

    HW (Fluent API)

--- 2024-09-23 - HW (Fluient API)/Program.cs
+++ 2024-09-23 - HW (Fluient API)/Program.cs
@@ -21,6 +21,33 @@ namespace _2024_09_23___HW__Fluient_API_
             {
                 WriteLine($"| Name : {item.Name}");
             }
+            WriteLine("\nShops :");
+            foreach (var item in GetDbContext().Shops)
+            {
+                Write($"| Name : {item.Name} ,  Address : {item.Address} ,  ");
+                if (item.ParkingArea != null) WriteLine($"Parking area : {item.ParkingArea}");
+            }
+            WriteLine("\nWorkers :");
+            foreach (var item in GetDbContext().Workers)
+            {
+                WriteLine($"| Fullname : {item.Name} {item.Surname} ,  Email : {item.Email} ,  " +
+                    $"Phone number : {item.PhoneNumber}");
+            }
+            WriteLine("\nPositions :");
+            foreach (var item in GetDbContext().Positions)
+            {
+                WriteLine($"| Name : {item.Name}");
+            }
+            WriteLine("\nProducts :");
+            foreach (var item in GetDbContext().Products)
+            {
+                WriteLine($"| Name : {item.Name} ,  Price : {item.Price} ,  Discount : {item.Discount}");
+            }
+            WriteLine("\nCategories :");
+            foreach (var item in GetDbContext().Categories)
+            {
+                WriteLine($"| Name : {item.Name}");
+            }
         }
     }
 }
